9f6d3a8832263c39ecb8456749d334fe465d1b98
hafiznu <hafiznuhafiznu@gmail.com>
2025-05-03 21:12:24 +0700

    Add files via upload

---
+++ hafiz.js
@@ -0,0 +1,10 @@
+let panjang = 20;
+let lebar = 5;
+
+// Menghitung luas persegi panjang
+let luas = panjang * lebar;
+console.log("Luas persegi panjang = " + luas);
+
+// Menghitung keliling persegi panjang
+let keliling = 2 * (panjang + lebar);
+console.log("Keliling persegi panjang = " + keliling);
