c8810bd60791892247143b8d110996399a80417a
hafiznu <hafiznuhafiznu@gmail.com>
2025-05-29 22:30:39 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-function konversi() {
-  const input = document.getElementById("decimalInput").value;
-  const decimal = parseInt(input);
+// Tangkap tombol berdasarkan class
+const signupBtn = document.querySelector(".signup-btn");
+const loginBtn = document.querySelector(".login-btn");
 
-  if (isNaN(decimal)) {
-    alert("Masukkan Angka yang benar!");
-    return;
-  }
+// Tambahkan event listener ke tombol REGISTER
+signupBtn.addEventListener("click", function () {
+  alert("Website Dalam Perbaikan");
+});
 
-  const biner = decimal.toString(2);
-  const oktal = decimal.toString(8);
-  const hex = decimal.toString(16).toUpperCase();
+// Tambahkan event listener ke tombol LOGIN
+loginBtn.addEventListener("click", function () {
+  alert("Website Dalam Perbaikan");
+});
 
-  alert(`Biner : ${biner}\nOktal : ${oktal}\nHeksadesimal : ${hex}`);
-}
+const words = ["HOROR", "ROMANCE", "COMEDY", "PALEMBANG"];
+let index = 0;
+
+const wordElement = document.querySelector(".changing-word");
+
+setInterval(() => {
+  index = (index + 1) % words.length; // loop balik ke awal
+  wordElement.textContent = words[index];
+}, 2000); // setiap 2000ms = 2 detik
